57ca7ab852e9dca4e779f2a8e17db7e5c1880213
MrkTheCoder <mrezak@gmail.com>
2022-02-25 16:48:24 +0330

    Complete Repository Pattern.

--- BookLibrary_Backend/Core/Core.Common.Interfaces/Data/IRepositoryBase.cs
+++ BookLibrary_Backend/Core/Core.Common.Interfaces/Data/IRepositoryBase.cs
@@ -3,11 +3,7 @@ using Core.Common.Interfaces.Entities;
 
 namespace Core.Common.Interfaces.Data
 {
-    public interface IRepositoryBase
-    {
-    }
-
-    public interface IRepositoryBase<TEntity> : IRepositoryBase
+    public interface IRepositoryBase<TEntity> : IRepository
         where TEntity : class, IIdentifiableEntity, new()
     {
         TEntity GetById(int id);
---
+++ BookLibrary_Backend/Data/BookLibrary.DataAccess.SQLite/Repositories/RepositoryBase.cs
@@ -0,0 +1,9 @@
+﻿using Core.Common.Interfaces.Entities;
+
+namespace BookLibrary.DataAccess.SQLite.Repositories
+{
+    public abstract class RepositoryBase<TEntity> : RepositoryBase<TEntity, BookLibraryDbContext>
+        where TEntity : class, IIdentifiableEntity, new()
+    {
+    }
+}
