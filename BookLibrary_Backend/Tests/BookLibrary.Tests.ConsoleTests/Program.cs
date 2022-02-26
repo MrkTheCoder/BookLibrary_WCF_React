5cdebdf9e222820d9056d7f5a8de6a957a47c12c
MrkTheCoder <mrezak@gmail.com>
2022-02-26 13:02:20 +0330

    Add BookRepositoryIntegrationTest

--- BookLibrary_Backend/Tests/BookLibrary.Tests.ConsoleTests/Program.cs
+++ BookLibrary_Backend/Tests/BookLibrary.Tests.ConsoleTests/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
+using BookLibrary.DataAccess.SQLite.Repositories;
+
 namespace BookLibrary.Tests.ConsoleTests
 {
     class Program
     {
         static void Main(string[] args)
         {
+            BookRepositoryIntegrationTests();
+        }
 
+        private static void BookRepositoryIntegrationTests()
+        {
+            var bookRepository = new BookRepository();
+
+            var books = bookRepository.GetAll();
+
+            foreach (var book in books)
+            {
+                Console.WriteLine($"Id: \t{book.Id}\n\r" +
+                                  $"ISBN: \t{book.Isbn}\n\r" +
+                                  $"Title: \t{book.Title}\n\r" +
+                                  $"EntityId: \t{book.EntityId}");
+                if (book.Id != book.EntityId)
+                    throw new Exception("In each Entity, EntityId property should be equal to its Id property.");
+                Console.WriteLine();
+            }
         }
     }
 }
