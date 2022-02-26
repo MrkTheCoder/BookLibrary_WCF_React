a6da907757caf189ffef796123965ff1a06d212c
MrkTheCoder <mrezak@gmail.com>
2022-02-26 12:46:10 +0330

    Add Test.Console project for Integration tests.

---
+++ BookLibrary_Backend/Tests/BookLibrary.Tests.ConsoleTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+namespace BookLibrary.Tests.ConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
