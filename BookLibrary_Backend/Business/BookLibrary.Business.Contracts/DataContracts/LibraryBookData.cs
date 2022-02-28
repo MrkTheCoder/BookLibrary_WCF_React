1ed5cc70b43c9271c8f3cf65f1f3876541d11087
MrkTheCoder <mrezak@gmail.com>
2022-02-28 15:23:58 +0300

    Add first and simple Data and Service contracts for Library Books.

---
+++ BookLibrary_Backend/Business/BookLibrary.Business.Contracts/DataContracts/LibraryBookData.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace BookLibrary.Business.Contracts.DataContracts
+{
+    [DataContract]
+    public class LibraryBookData
+    {
+        [DataMember] 
+        public int Id { get; set; }
+
+        [DataMember] 
+        public string Isbn { get; set; }
+
+        [DataMember] 
+        public string Title { get; set; }
+
+        [DataMember] 
+        public bool IsAvailable { get; set; }
+    }
+}
---
+++ BookLibrary_Backend/Business/BookLibrary.Business.Contracts/ServiceContracts/IBookService.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+using BookLibrary.Business.Contracts.DataContracts;
+
+namespace BookLibrary.Business.Contracts.ServiceContracts
+{
+    [ServiceContract]
+    public interface IBookService
+    {
+        [OperationContract]
+        LibraryBookData[] GetLibraryBooks();
+    }
+}
