856287329fae75086fe20e61f03e2463c3a98470
MrkTheCoder <mrezak@gmail.com>
2022-03-09 17:02:18 +0330

    Add Host.Wcf.ConsoleHost library.

---
+++ BookLibrary_Backend/Host/BookLibrary.Hot.Wcf.ConsoleHost/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Hot.Wcf.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
