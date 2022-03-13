50206b70dd5f21858c847eb43a1d7179244ac56e
MrkTheCoder <mrezak@gmail.com>
2022-03-13 16:25:50 +0330

    Add Host.Wcf.ConsoleHost references.

--- BookLibrary_Backend/Host/BookLibrary.Hot.Wcf.ConsoleHost/Program.cs
+++ BookLibrary_Backend/Host/BookLibrary.Hot.Wcf.ConsoleHost/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BookLibrary.Hot.Wcf.ConsoleHost
+﻿namespace BookLibrary.Hot.Wcf.ConsoleHost
 {
     class Program
     {
