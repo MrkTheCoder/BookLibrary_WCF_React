188656ca5ecec5de73f7394e131d5910a19ce238
MrkTheCoder <mrezak@gmail.com>
2022-03-13 16:48:55 +0330

    Implement Wcf.ConsoleHost

--- BookLibrary_Backend/Host/BookLibrary.Hot.Wcf.ConsoleHost/Program.cs
+++ BookLibrary_Backend/Host/BookLibrary.Hot.Wcf.ConsoleHost/Program.cs
@@ -1,9 +1,49 @@
-﻿namespace BookLibrary.Hot.Wcf.ConsoleHost
+﻿using BookLibrary.Business.Services.Managers;
+using System.ServiceModel;
+
+namespace BookLibrary.Hot.Wcf.ConsoleHost
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // If Unattended Process need Principal Permission Security!?
+            // Then we should define it here
+
+            var bookHost = new ServiceHost(typeof(BookManager));
+
+            StartHost(bookHost, nameof(BookManager));
+
+            // Unattended process place here (Timer Start) ...
+
+            System.Console.WriteLine("");
+            System.Console.WriteLine("Press [Enter] to close app.");
+            System.Console.ReadLine();
+            System.Console.WriteLine("");
+
+            // Timer Stop for Unattended process ...
+            StopHost(bookHost, nameof(BookManager));
         }
+
+        private static void StartHost(ServiceHost host, string serviceName)
+        {
+            host.Open();
+            System.Console.WriteLine($"Service: '{serviceName}' is started.");
+            foreach (var endpoint in host.Description.Endpoints)
+            {
+                System.Console.WriteLine($"\tAddress:\t{endpoint.Address}");
+                System.Console.WriteLine($"\tBinding:\t{endpoint.Binding?.Name}");
+                System.Console.WriteLine($"\tContract:\t{endpoint.Contract.ConfigurationName}");
+            }
+
+            System.Console.WriteLine("");
+        }
+
+        private static void StopHost(ServiceHost host, string serviceName)
+        {
+            host.Close();
+            System.Console.WriteLine($"Service: '{serviceName}' is stop.");
+        }
+
     }
 }
