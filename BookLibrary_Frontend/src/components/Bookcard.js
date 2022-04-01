ad8a5cb66abf979e8c5a2be85fdc6070acf54cbd
Ali batashi <batashi.ali@gmail.com>
2022-04-01 21:54:48 +0430

    Added images to bookCards

--- BookLibrary_Frontend/src/components/Bookcard.js
+++ BookLibrary_Frontend/src/components/Bookcard.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./style.css";
 
 export default function Bookcard(props) {
   const book = props.book;
   return props.book ? (
-    <div className="bookCards">
-      <Card data-testid="card" className=" my-3">
-        <div className="cardBody">
-          <Card.Body className="bookCards">
-            <Card.Title className="cardTitle" data-testid="title">
+    <div className="customCard">
+      <Card data-testid="card" className="my-3 " id="cardFrame">
+        <Card.Body className="bookCards">
+          <div>
+            <Card.Title id="cardTitle" data-testid="title">
               {book.Title}
             </Card.Title>
 
@@ -42,11 +42,16 @@ export default function Bookcard(props) {
                 More Details
               </Link>
             </Card.Link>
-          </Card.Body>
+          </div>
+        </Card.Body>
+        <div className="cardImg">
+          <Card.Img
+            variant="bottom"
+            data-testid="image"
+            src={book.CoverLink}
+            alt={book.Isbn}
+          />
         </div>
-        <span className="cardImage">
-          <img data-testid="image" src={book.CoverLink} alt={book.Isbn} />
-        </span>
       </Card>
     </div>
   ) : (
