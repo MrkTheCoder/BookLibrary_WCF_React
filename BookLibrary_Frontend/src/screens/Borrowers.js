f5cecf86c0fd9bc609d9c8adce5245601e208ab5
Ali batashi <batashi.ali@gmail.com>
2022-04-08 17:09:22 +0430

    Added a placeholder chart to boowers.js

--- BookLibrary_Frontend/src/screens/Borrowers.js
+++ BookLibrary_Frontend/src/screens/Borrowers.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Card, Container, Col, ListGroup, Button } from "react-bootstrap";
+import {
+  Card,
+  Container,
+  Col,
+  ListGroup,
+  Button,
+  Table,
+} from "react-bootstrap";
 import { listBorrowers } from "../actions/borrowersActions";
 
 function Borrowers() {
@@ -12,7 +19,40 @@ function Borrowers() {
     borrowers.length == 0 && dispatch(listBorrowers());
     console.log(borrowers);
   }, [dispatch]);
-  return <div>Borrowers</div>;
+  return (
+    <div>
+      <Table responsive striped bordered hover variant="light">
+        <thead>
+          <tr>
+            <th>#</th>
+            {Array.from({ length: 12 }).map((_, index) => (
+              <th key={index}>Table heading</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            {Array.from({ length: 12 }).map((_, index) => (
+              <td key={index}>Table cell {index}</td>
+            ))}
+          </tr>
+          <tr>
+            <td>2</td>
+            {Array.from({ length: 12 }).map((_, index) => (
+              <td key={index}>Table cell {index}</td>
+            ))}
+          </tr>
+          <tr>
+            <td>3</td>
+            {Array.from({ length: 12 }).map((_, index) => (
+              <td key={index}>Table cell {index}</td>
+            ))}
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
 }
 
 export default Borrowers;
