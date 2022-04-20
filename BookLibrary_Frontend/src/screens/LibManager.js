c2d109d38046afb8d8ab50962791600ad9ee355c
Ali batashi <batashi.ali@gmail.com>
2022-04-20 21:35:32 +0430

    Added 2 empty files, to be completed

---
+++ BookLibrary_Frontend/src/screens/LibManager.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Col } from "react-bootstrap";
+
+function LibManager() {
+  return (
+    <div>
+      <Col></Col>
+    </div>
+  );
+}
+
+export default LibManager;
