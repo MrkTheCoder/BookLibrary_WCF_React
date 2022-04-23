6084a2e7cdf08306685c8f26ecdb649945513fc1
Ali batashi <batashi.ali@gmail.com>
2022-04-23 15:29:43 +0430

    Removed un used links from header

--- BookLibrary_Frontend/src/components/Header.js
+++ BookLibrary_Frontend/src/components/Header.js
@@ -23,24 +23,9 @@ export default function Header() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Books</Nav.Link>
-
-            <NavDropdown
-              title={`Ali batashi${(<i className="fas fa-solid"></i>)}`}
-              id="collasible-nav-dropdown"
-            >
-              <LinkContainer to={"/user"}>
-                <NavDropdown.Item>Profile</NavDropdown.Item>
-              </LinkContainer>
-              <LinkContainer to={"/userbooks"}>
-                <NavDropdown.Item>Books</NavDropdown.Item>
-              </LinkContainer>
+            <NavDropdown title={`Manage`} id="collasible-nav-dropdown">
               <LinkContainer to={"/admin/borrowers"}>
                 <NavDropdown.Item>Borrowers list</NavDropdown.Item>
-              </LinkContainer>
-              <NavDropdown.Divider />
-              <LinkContainer to={"/logout"}>
-                <NavDropdown.Item>Logout</NavDropdown.Item>
               </LinkContainer>
             </NavDropdown>
           </Nav>
